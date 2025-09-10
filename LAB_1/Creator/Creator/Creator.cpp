513655449bcca9af18a06a4656d8a800f7bbf886
Daria Movchanskaya <dashamovchanskaya@gmail.com>
2025-09-11 00:10:02 +0300

    Added files with Creator and Reporter

---
+++ LAB_1/Creator/Creator/Creator.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include "employee.h"
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "rus");
+
+    if (argc != 3) {
+        std::cerr << "Usage: Creator <filename> <record_count>\n";
+        return 1;
+    }
+
+    const char* filename = argv[1];
+    int count = std::atoi(argv[2]);
+
+    std::ofstream fout(filename, std::ios::binary);
+    if (!fout) {
+        std::cerr << "Ошибка при открытии файла для записи.\n";
+        return 1;
+    }
+
+    for (int i = 1; i <= count; ++i) {
+        employee emp;
+        std::cout << "Введите данные для сотрудника #" << i << ":\n";
+
+        std::cout << "Введите номер: ";
+        std::cin >> emp.num;
+
+        std::cin.ignore(); // очистка буфера после предыдущего ввода
+        std::cout << "Введите имя (до 9 символов, можно с пробелами): ";
+        std::cin.getline(emp.name, sizeof(emp.name));
+
+        std::cout << "Введите часы: ";
+        std::cin >> emp.hours;
+
+        fout.write(reinterpret_cast<char*>(&emp), sizeof(emp));
+    }
+
+    fout.close();
+    std::cout << "Файл " << filename << "успешно создан!" << "\n";
+    return 0;
+
+}
+
---
+++ LAB_1/Reporter/Reporter/Reporter.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "employee.h"
+
+int main(int args, char* arg[])
+{
+    setlocale(LC_ALL, "rus");
+
+    if (args != 4) {
+        std::cerr << "Usage: Reporter <input_binary_file> <output_report_file> <hourly_rate>\n";
+        return 1;
+    }
+
+    const char* binfileName = arg[1];
+    const char* reportfileName = arg[2];
+    double payment = std::atof(arg[3]);
+
+    std::ifstream fin(binfileName, std::ios::binary);
+    if (!fin) {
+        std::cerr << "Ошибка при открытие входного файла!\n";
+        return 1;
+    }
+
+    std::ofstream fout(reportfileName);
+    if (!fout) {
+        std::cerr << "Ошибка при открытии файла для записис!\n";
+        return 1;
+    }
+
+    fout << "Отчет по файлу " << binfileName << "\n";
+    fout << std::left << std::setw(18) << "Номер сотрудника, " << std::setw(16) << "Имя сотрудника, " << std::setw(8) << "Часы, " << std::setw(8) << "Зарплата\n";
+
+    employee emp;
+    while (fin.read(reinterpret_cast<char*>(&emp), sizeof(emp))) {
+        double salary = payment * emp.hours;
+        fout << std::left << std::setw(18) << emp.num << std::setw(16) << emp.name << std::setw(8) << emp.hours << std::setw(8) << salary << "\n";
+    }
+
+    fin.close();
+    fout.close();
+
+    std::cout << "Файл " << reportfileName << " успешно создан!\n";
+    return 0;
+}
+
+
